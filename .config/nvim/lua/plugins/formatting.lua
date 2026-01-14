9127adbebbcd531b72ebc898e9c9870084b15e5d
rednokay <207755534+rednokay@users.noreply.github.com>
2026-01-14 12:18:10 +0100

    Use black formatter when saving python files

---
+++ .config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },           -- load right before saving
+    opts = {
+      format_on_save = {
+        timeout_ms = 1500,               -- kill if takes too long
+        lsp_format = "fallback",         -- if no formatter → try LSP
+      },
+
+      formatters_by_ft = {
+        python = { "black" },
+      },
+    },
+  },
+}
